074ace99fd584d15312390da1cb34bc2ad6bce1c
Kacper Hryszkiewicz <k.hryszkiewicz2@wp.pl>
2018-07-19 14:15:19 +0200

    funkcje

---
+++ warsztaty/jswstep/js/funkcje.js
@@ -0,0 +1,22 @@
+//deklaracja funkcji
+//function wypiszDoKonsoli() {
+    'use strict';
+//    console.log('123');
+//}
+
+//wypiszDoKonsoli();
+ 
+
+//wyrazenie funkcyjne
+const wypiszDoKonsoli = function() {
+    console.log('123')
+}
+
+wypiszDoKonsoli();
+
+//funkcje parametrowe
+function dodawanie(parametr1, parametr2) {
+    console.log(parametr1 + parametr2);
+}
+
+dodawanie(1, 2);
---
+++ warsztaty/jswstep/js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//komentarze ctrl + / lub cmd + / - komentarz liniowy
+
+/* komentarz blokowy
+//mozna aczyc komentarz liniowy z blokowym, ale nie w strone przeciwna
+*/
+
+//deklaracja zmiennej to zarzerwowanie komorki pamieci dla jakiejs wartosci
+let zmienna;
+
+//ini jalizaja zminnej to przypisanie wartosci do nazwanego miejsca w pamieci
+const numberOfMonths = 12;
+
+//numberOfM(onths = 10; - nie mozna zmieniac wartosci (const);
+console.log('123');
